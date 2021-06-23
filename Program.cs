7242be3100fbd24fabf0e6c6cfd7963342709076
[Sona] <[sona16061999@gmail.com]>
2021-06-23 13:27:49 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNo = 12;
+            int myNo1 = 24;
+            Console.WriteLine(myNo + myNo1);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
